04eb2b3ce92f9e73b05ea669afd23c309f112bbf
Jagoda Lewińska <jagodal0205@gmail.com>
2023-01-30 15:58:33 +0100

    adding functions

--- js/kantor.js
+++ js/kantor.js
@@ -1,32 +1,48 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+ 
+  {
+    const calculatorResult =(amount, currency) => {
+        const EUR = 4.6709;
+        const USD = 4.3970;
+        const CHF = 4.7437;
+    
+        switch (currency) {
+            case "EUR":
+                return amount / EUR;
+                
+            case "USD":
+                return amount / USD;
+               
+            case "CHF":
+                return amount / CHF;
+                
+        }
+    };
 
-let EUR = 4.6709;
-let USD = 4.3970;
-let CHF = 4.7437;
+    const updateResultText = (amount, result, currency) => {
+        const resultElement = document.querySelector(".js-result");
+        resultElement.innerHTML = `${amount.toFixed(2)} PLN = ${result.toFixed} ${currency}`;
+    };
 
-formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
+    const onFormSubmit = (event) => {
+        event.preventDefault();
+        
+        const amountElement = document.querySelector(".js-amount");
+        const currencyElement = document.querySelector(".js-currency");
+        
+        
+        const amount = +amountElement.value;
+        const currency = currencyElement.value;
+    
+        const result = calculatorResult(amount, currency);
 
-    let amount = Number(amountElement.value);
-    let currency = currencyElement.value;
+    };
+    
+        const init = () => {
+            const formElement = document.querySelector(".js-form");
+                  
+        formElement.addEventListener("submit", onFormSubmit);
+    };
 
-    let result;
+        init();
 
-    switch (currency) {
-        case "EUR":
-            result = amount / EUR;
-            break;
-
-        case "USD":
-            result = amount / USD;
-            break;
-
-        case "CHF":
-            result = amount / CHF;
-            break;
-    }
-    resultElement.innerText = `${result.toFixed(2)} ${currency}`;
-});
+ }
